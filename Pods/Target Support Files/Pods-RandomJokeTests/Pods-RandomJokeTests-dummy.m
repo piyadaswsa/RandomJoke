066d7a0eb6fe854a76d5d4cb60e5c1492d09dfa1
Piyada Suwansa-ard <swsa.piyada@gmail.com>
2019-08-09 10:39:18 +0700

    pod files

---
+++ Pods/Target Support Files/Pods-RandomJokeTests/Pods-RandomJokeTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RandomJokeTests : NSObject
+@end
+@implementation PodsDummy_Pods_RandomJokeTests
+@end
---
+++ Pods/Target Support Files/Pods-RandomJokeUITests/Pods-RandomJokeUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RandomJokeUITests : NSObject
+@end
+@implementation PodsDummy_Pods_RandomJokeUITests
+@end
